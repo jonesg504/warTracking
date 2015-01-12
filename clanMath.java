a7fe2d5d8ca54c730e8134b473858c637822a203
Griffin Jones <jonesg504@gmail.com>
2015-01-12 14:24:06 -0500

    Added a block comment

--- clanMath.java
+++ clanMath.java
@@ -12,10 +12,10 @@ public class clanMath {
 		}
 	}
 	public static void main(String[] args) {
-		Player p1 = new Player("Griffin", 3);
+		/*Player p1 = new Player("Griffin", 3);
 		Player p2 = new Player("Jeffrey", 6);
 		Player[] players = {p1, p2};
 		Clan suns = new Clan(players);
-		clanStats(suns);
+		clanStats(suns);*/
 	}
 }
